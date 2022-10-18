4edd6244bf7f616c3298f4399b557728e4b4dec6
Nick Gal <nickgal@me.com>
2022-10-17 22:06:29 -0700

    add t3d ff zip download

--- components/T3dFile.vue
+++ components/T3dFile.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
 
-    <input type="file" @change="parseT3dFile($event.target.files[0])">
-
+    <input type="file" @change="fileChange">
+    <a v-if="t3d && t3dFilename" href="#" @click.prevent="downloadZip">Download all</a>
     <div v-if="t3d">
       <div class="grid grid-cols-3 gap-4">
         <fast-file-record v-for="file in t3d.files" :key="file.i" :file="file"></fast-file-record>
@@ -15,6 +15,8 @@
 import Fastfile from '../static/files/lib/Fastfile'
 import T3dFastFile from '../static/files/lib/T3dFastFile'
 import KaitaiStream from 'kaitai-struct/KaitaiStream'
+import JSZip from 'jszip'
+import { saveAs } from 'file-saver'
 
 export default {
   props: {
@@ -25,6 +27,7 @@ export default {
   },
   data() {
     return {
+      t3dFilename: null,
       t3d: null,
     }
   },
@@ -44,9 +47,17 @@ export default {
       }
 
       return parser;
+    },
+    t3dZipFilename() {
+      return `${this.t3dFilename}.zip`
     }
   },
   methods: {
+    fileChange(event) {
+      const file = event.target.files[0];
+      this.t3dFilename = file.name;
+      this.parseT3dFile(file);
+    },
     async parseT3dFile(file) {
       const arrayBuffer = await file.arrayBuffer();
       this.parseT3d(arrayBuffer);
@@ -55,6 +66,13 @@ export default {
       const stream = new KaitaiStream(buffer);
       this.t3d = new this.parser(stream);
       console.dir(this.t3d)
+    },
+    downloadZip() {
+      const zip = new JSZip();
+      this.t3d.files.forEach(file => zip.file(file.filename, file.body));
+      zip.generateAsync({type:"blob"}).then((blob) => {
+        saveAs(blob, this.t3dZipFilename);
+      });
     },
   }
 }
